c88ce7f7fc9421f3bac8cc5bd1af10593f23eb7c
Juan Valencia <juanda.vid1996@hotmail.com>
2022-04-05 02:44:04 -0500

    Path_operations_structure_user

--- main.py
+++ main.py
@@ -2,12 +2,13 @@
 from uuid import UUID
 from datetime import date
 from datetime import datetime
-from typing import Optional
+from typing import Optional, List
 
 # Pydantic
 from pydantic import BaseModel
 from pydantic import EmailStr
 from pydantic import Field 
+from fastapi import status
 
 # FastAPI
 from fastapi import FastAPI
@@ -53,10 +54,83 @@ class Tweet(BaseModel):
     )
     create_at: datetime = Field(default = datetime.now())
     update_at: Optional[datetime] = Field(default = None)
-    By: User = Field(...)
+    By: User = Field(...) # heredo los datos del usuario que estan en la clase User
 
-
+# Path Operations
 
 @app.get(path = "/")
 def home():
     return {"Twitter API": "Working"}
+
+## User
+
+### Signup
+@app.post(
+    path = "/signup",
+    response_model = User, # Respondemos con la información de un usuario -- hereda de la class User
+    status_code = status.HTTP_201_CREATED,
+    summary = "Register a User",
+    tags = ["User"]
+)
+def signup():
+    pass
+
+### Login
+@app.post(
+    path = "/login",
+    response_model = User, # Respondemos con la información de un usuario -- hereda de la class User
+    status_code = status.HTTP_200_OK,
+    summary = "Login User Success",
+    tags = ["User"]
+)
+def login():
+    pass
+
+### Users
+@app.get(
+    path = "/users",
+    response_model = List[User], # Respondemos con una lista de los usuarios -- hereda de la class User
+    status_code = status.HTTP_200_OK,
+    summary = "Show All User",
+    tags = ["User"]
+)
+def show_all_users():
+    pass
+
+### User id
+@app.get(
+    path = "/users/{user_id}",
+    response_model = User, # Traemos la data del usuario -- hereda de la class User
+    status_code = status.HTTP_200_OK,
+    summary = "Show a User",
+    tags = ["User"]
+)
+def show_a_users():
+    pass
+
+### Delete User
+@app.delete(
+    path = "/users/{user_id}/delete",
+    response_model = User, # Traemos la data del usuario -- hereda de la class User
+    status_code = status.HTTP_200_OK,
+    summary = "Delete a User",
+    tags = ["User"]
+)
+def delete_a_users():
+    pass
+
+### update User
+@app.put(
+    path = "/users/{user_id}/update",
+    response_model = User, # Traemos la data del usuario -- hereda de la class User
+    status_code = status.HTTP_200_OK,
+    summary = "Update a User",
+    tags = ["User"]
+)
+def update_a_users():
+    pass
+
+
+
+
+## Tweet
